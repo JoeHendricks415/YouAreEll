0beb90990b30c6536486b5615898decae52cd673
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-03-22 00:35:27 -0400

    Able to get with unirest but not yet speak

---
+++ Client/src/main/java/Id.java
@@ -0,0 +1,47 @@
+public class Id {
+
+    private String userid;
+    private String name;
+    private String github;
+
+    public Id(String userid, String name, String github) {
+        this.userid = userid;
+        this.name = name;
+        this.github = github;
+
+    }
+
+    public Id(String name, String github){
+        this.name = name;
+        this.github = github;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    @Override
+    public String toString(){
+        return "User ID: " + this.userid + "Name: " + this.name +  "GitHub: " + this.github;
+    }
+}
--- Client/src/main/java/YouAreEll.java
+++ Client/src/main/java/YouAreEll.java
@@ -1,3 +1,16 @@
+
+
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.json.*;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
 public class YouAreEll {
 
     YouAreEll() {
@@ -7,17 +20,42 @@ public class YouAreEll {
         YouAreEll urlhandler = new YouAreEll();
         System.out.println(urlhandler.MakeURLCall("/ids", "GET", ""));
         System.out.println(urlhandler.MakeURLCall("/messages", "GET", ""));
+
     }
 
     public String get_ids() {
+
         return MakeURLCall("/ids", "GET", "");
     }
 
     public String get_messages() {
+
         return MakeURLCall("/messages", "GET", "");
     }
 
     public String MakeURLCall(String mainurl, String method, String jpayload) {
+        String magicURL = "http://zipcode.rocks:8085" + mainurl;
+        GetRequest request = Unirest.get(magicURL);
+        Id idTest = new Id("JoeyBiscuits", "JoeHendricks415");
+
+        if (method.equals("GET")) {
+            try {
+                return request.asJson().getBody().toString();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (method.equals("POST")){
+            try {
+                return Unirest.post(magicURL).body(jpayload).asJson().getBody().toString();
+            }
+            catch(UnirestException e){
+                e.printStackTrace();
+            }
+        }
         return "nada";
+
     }
+
 }
+
